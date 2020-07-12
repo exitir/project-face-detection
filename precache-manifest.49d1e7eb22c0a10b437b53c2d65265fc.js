ca982ef5b522ab6cdaf85adf0a8e9963cdf89ee0
exitir@gmail.com <exitir@gmail.com>
2020-07-13 03:11:13 +0530

    Updates

---
+++ precache-manifest.49d1e7eb22c0a10b437b53c2d65265fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c1c6907c16e6bbe6082ce6fe27b2a68",
+    "url": "/project-face-detection/index.html"
+  },
+  {
+    "revision": "9ceca3f8e76474576a7b",
+    "url": "/project-face-detection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1ed5a36eede488ff4cc2",
+    "url": "/project-face-detection/static/css/main.6dac85b7.chunk.css"
+  },
+  {
+    "revision": "9ceca3f8e76474576a7b",
+    "url": "/project-face-detection/static/js/2.996dc824.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-face-detection/static/js/2.996dc824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed5a36eede488ff4cc2",
+    "url": "/project-face-detection/static/js/main.44576e94.chunk.js"
+  },
+  {
+    "revision": "fe44d87e9dc75798bdff",
+    "url": "/project-face-detection/static/js/runtime-main.7c2ca479.js"
+  }
+]);
